ed156b642b9b317acc548ff0074e9818b7bde68f
Spencer Johnson <sljohnson2222@gmail.com>
2022-03-15 15:48:04 -0500

    commit tree 190494e173439ca13a833c80328e68a241cb8d81

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister
+    attr_accessor :discount
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price)
+
+  end
+
+  def apply_discount
+
+  end
+
+  def items
+    
+  end
+
+
+
+
+end
